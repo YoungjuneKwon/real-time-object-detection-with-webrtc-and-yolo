ca368f914591a220a847b0bb25522bae49a2f182
Youngjune Kwon <yjkwon@winm2m.com>
2024-01-02 23:40:49 +0000

    resolved pylint and cleared unused lines

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-# pylint: disable=all
+""" WebRTC webcam demo """
 import argparse
 import asyncio
 import json
 import logging
 import os
-import platform
+import cv2
 
 from aiohttp import web
-import cv2
 import numpy as np
 from av import VideoFrame
 from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
-from aiortc.contrib.media import MediaPlayer, MediaRelay
 
 ROOT = os.path.dirname(__file__)
+MODEL = "yolov7-tiny_480x640.onnx"
 
-relay = None
-webcam = None
-
-model = "yolov7-tiny_480x640.onnx"
-
+# pylint: disable=too-many-instance-attributes
 class YOLOVideoStreamTrack(VideoStreamTrack):
     """
     A video track thats returns camera track with annotated detected objects.
@@ -31,15 +26,16 @@ class YOLOVideoStreamTrack(VideoStreamTrack):
 
         self.track = track
 
-        # Initialize model
-        self.net = cv2.dnn.readNet(model)
-        input_shape = os.path.splitext(os.path.basename(model))[0].split('_')[-1].split('x')
+        # Initialize MODEL
+        self.net = cv2.dnn.readNet(MODEL)
+        input_shape = os.path.splitext(os.path.basename(MODEL))[0].split('_')[-1].split('x')
         self.input_height = int(input_shape[0])
         self.input_width = int(input_shape[1])
 
-        self.class_names = list(map(lambda x: x.strip(), open('coco.names', 'r').readlines()))
+        with open('coco.names', 'r', encoding='utf-8') as f:
+            self.class_names = list(map(lambda x: x.strip(), f.readlines()))
         self.colors = np.random.default_rng(3).uniform(0, 255, size=(len(self.class_names), 3))
-                
+
         self.output_names = self.net.getUnconnectedOutLayersNames()
         self.input_frame = None
         self.flag_proceed = False
@@ -48,12 +44,14 @@ class YOLOVideoStreamTrack(VideoStreamTrack):
         asyncio.create_task(self.process_output_frame())
 
     async def store_input_frame(self):
+        """ Store input frame """
         while True:
             frame = await self.track.recv()
             self.input_frame = frame.to_ndarray(format="bgr24")
             self.flag_proceed = False
 
     async def process_output_frame(self):
+        """ Process output frame """
         while self.input_frame is None:
             await asyncio.sleep(0.2)
         while True:
@@ -71,77 +69,86 @@ class YOLOVideoStreamTrack(VideoStreamTrack):
             self.flag_proceed = True
 
     async def recv(self):
+        """ Receive frame """
         while self.output_frame is None:
             await asyncio.sleep(0.2)
         return self.output_frame
- 
+
     def prepare_input(self, image):
+        """ Prepare input image """
+        # pylint: disable=attribute-defined-outside-init
         self.img_height, self.img_width = image.shape[:2]
-        
+
         input_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        
+
         # Resize input image
         input_img = cv2.resize(input_img, (self.input_width, self.input_height))
-        
+
         # Scale input pixel values to 0 to 1
         return input_img
 
     def detect(self, frame):
+        """ Detect objects """
         input_img = self.prepare_input(frame)
         blob = cv2.dnn.blobFromImage(input_img, 1 / 255.0)
         # Perform inference on the image
         self.net.setInput(blob)
         # Runs the forward pass to get output of the output layers
         outputs = self.net.forward(self.output_names)
-        
+
         boxes, scores, class_ids = self.process_output(outputs)
         return boxes, scores, class_ids
 
     def process_output(self, output):
+        """ Process output """
         predictions = np.squeeze(output[0])
-        
+
         # Filter out object confidence scores below threshold
         obj_conf = predictions[:, 4]
         predictions = predictions[obj_conf > self.conf_threshold]
         obj_conf = obj_conf[obj_conf > self.conf_threshold]
-        
+
         # Multiply class confidence with bounding box confidence
         predictions[:, 5:] *= obj_conf[:, np.newaxis]
-        
+
         # Get the scores
         scores = np.max(predictions[:, 5:], axis=1)
-        
+
         # Filter out the objects with a low score
         valid_scores = scores > self.conf_threshold
         predictions = predictions[valid_scores]
         scores = scores[valid_scores]
-        
+
         # Get the class with the highest confidence
         class_ids = np.argmax(predictions[:, 5:], axis=1)
-        
+
         # Get bounding boxes for each object
         boxes = self.extract_boxes(predictions)
-        
+
         # Apply non-maxima suppression to suppress weak, overlapping bounding boxes
-        indices = cv2.dnn.NMSBoxes(boxes.tolist(), scores.tolist(), self.conf_threshold, self.iou_threshold)
+        indices = cv2.dnn.NMSBoxes(boxes.tolist(), scores.tolist(),
+                              self.conf_threshold, self.iou_threshold)
         if len(indices) > 0:
             indices = indices.flatten()
-        
+
         return boxes[indices], scores[indices], class_ids[indices]
 
     def rescale_boxes(self, boxes):
-        input_shape = np.array([self.input_width, self.input_height, self.input_width, self.input_height])
+        """ Rescale boxes """
+        input_shape = np.array([self.input_width, self.input_height,
+                                self.input_width, self.input_height])
         boxes = np.divide(boxes, input_shape, dtype=np.float32)
         boxes *= np.array([self.img_width, self.img_height, self.img_width, self.img_height])
         return boxes
-    
+
     def extract_boxes(self, predictions):
+        """ Extract boxes """
         # Extract boxes from predictions
         boxes = predictions[:, :4]
-        
+
         # Scale boxes to original image dimensions
         boxes = self.rescale_boxes(boxes)
-        
+
         # Convert boxes to xywh format
         boxes_ = np.copy(boxes)
         boxes_[..., 0] = boxes[..., 0] - boxes[..., 2] * 0.5
@@ -149,10 +156,11 @@ class YOLOVideoStreamTrack(VideoStreamTrack):
         return boxes_
 
     def draw_detections(self, frame, boxes, scores, class_ids):
+        """ Draw detections """
         for box, score, class_id in zip(boxes, scores, class_ids):
             x, y, w, h = box.astype(int)
             color = self.colors[class_id]
-            
+
             # Draw rectangle
             cv2.rectangle(frame, (x, y), (x+w, y+h), color, thickness=2)
             label = self.class_names[class_id]
@@ -161,62 +169,43 @@ class YOLOVideoStreamTrack(VideoStreamTrack):
             cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, color, thickness=2)
         return frame
 
-
-def create_local_tracks():
-    global relay, webcam
-    options = {"framerate": "30", "video_size": "640x480"}
-    if relay is None:
-        if platform.system() == "Darwin":
-            webcam = MediaPlayer(
-                "default:none", format="avfoundation", options=options
-            )
-        elif platform.system() == "Windows":
-            webcam = MediaPlayer(
-                "video=Integrated Camera", format="dshow", options=options
-            )
-        else:
-            webcam = MediaPlayer("/dev/video0", format="v4l2", options=options)
-        relay = MediaRelay()
-    return relay.subscribe(webcam.video)
-
-
-async def index(request):
-    content = open(os.path.join(ROOT, "index.html"), "r").read()
+async def index(_):
+    """ Serve the client-side application """
+    with open(os.path.join(ROOT, "index.html"), "r", encoding='utf-8') as f:
+        content = f.read()
     return web.Response(content_type="text/html", text=content)
 
-async def javascript(request):
-    content = open(os.path.join(ROOT, "client.js"), "r").read()
+async def javascript(_):
+    """ Client JavaScript """
+    with open(os.path.join(ROOT, "client.js"), "r", encoding='utf-8') as f:
+        content = f.read()
     return web.Response(content_type="application/javascript", text=content)
 
-async def myIceServers(request):
-    content = open(os.path.join(ROOT, "myIceServers.json"), "r").read()
+async def my_ice_servers(_):
+    """ My ICE servers """
+    with open(os.path.join(ROOT, "myIceServers.json"), "r", encoding='utf-8') as f:
+        content = f.read()
     return web.Response(content_type="application/json", text=content)
 
-def add_event_handler_for_relay_track_on_peer_connection(pc):
-    relay = MediaRelay()
-    @pc.on("track")
-    def on_track(track):
-        print("Track %s received" % track.kind)
-        if track.kind == "video":
-            pc.addTrack(YOLOVideoStreamTrack(relay.subscribe(track)))
-
-async def offer(request):
+async def on_offer(request):
+    """ WebRTC offer """
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+    pcs.add(pc := RTCPeerConnection())
 
-    pc = RTCPeerConnection()
-    add_event_handler_for_relay_track_on_peer_connection(pc)
-    pcs.add(pc)
+    @pc.on("track")
+    def on_track(track):
+        if track.kind == "video":
+            pc.addTrack(YOLOVideoStreamTrack(track))
 
     @pc.on("connectionstatechange")
     async def on_connectionstatechange():
-        print("Connection state is %s" % pc.connectionState)
+        print(f"Connection state is {pc.connectionState}")
         if pc.connectionState == "failed":
             await pc.close()
             pcs.discard(pc)
 
     await pc.setRemoteDescription(offer)
-
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
 
@@ -227,15 +216,14 @@ async def offer(request):
         ),
     )
 
-
 pcs = set()
 
-async def on_shutdown(app):
+async def on_shutdown(_):
+    """ Close peer connections """
     # close peer connections
     coros = [pc.close() for pc in pcs]
     await asyncio.gather(*coros)
     pcs.clear()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="WebRTC webcam demo")
@@ -253,6 +241,6 @@ if __name__ == "__main__":
     app.on_shutdown.append(on_shutdown)
     app.router.add_get("/", index)
     app.router.add_get("/client.js", javascript)
-    app.router.add_post("/offer", offer)
-    app.router.add_get("/myIceServers", myIceServers)
+    app.router.add_post("/offer", on_offer)
+    app.router.add_get("/myIceServers", my_ice_servers)
     web.run_app(app, host=args.host, port=args.port)
